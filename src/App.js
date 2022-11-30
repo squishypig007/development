92e91da038e96483bb84d38accd193fe65f80297
isha <isha_mody@brown.edu>
2022-11-30 16:34:29 -0500

    everything breaks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import bakeryData from "./assets/bakery-data.json";
+import BakeryItem from "./components/BakeryItem";
+
+/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+bakeryData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
+/* ############################################################## */
 
 function App() {
+  const[favorites, setFavorites] = useState([]);
+  const[price, setPrice] = useState(0);
+  const[favoriteSelect, setfavoriteSelect] = useState(false);
+
+  const[displayItems, setDisplayItems] = useState(bakeryData);
+  const[rRatedItems, setRRatedItems] = useState([]);
+  const[type, setType] = useState("All");
+  
+  const[count, setCount] = useState(0);
+
+  useEffect(()=>{
+    let originalData = bakeryData;
+    console.log("before", originalData);
+    originalData = originalData.filter((movie)=>{
+      return movie.rated==="R"
+    })
+    console.log("after", originalData);
+    setDisplayItems(originalData);
+  },[type])
+  //sort based on views/popularity
+  //i'm doing and filtering (intersections)
+  //filter on Free/paid
+  //pg-13/r filter
+  //aggregator price of favorites - does not need to work w filter and sort
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Blockbuster Rentals</h1>
+      <div className="outermost_columns">
+        <div className="menu">
+        <h3>Filters</h3>
+        <br></br>
+        <form>
+        <div className="radio">
+          <label>
+            <input type="radio" value="option1" checked={true} />
+            Option 1
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="option2" />
+            Option 2
+          </label>
+        </div>
+        <div className="radio">
+          <label>
+            <input type="radio" value="option3" />
+            Option 3
+          </label>
+        </div>
+      </form>
+        </div>
+        <div className="wrapper_bakery_item">
+          {/* if({favoriteSelect===true}){
+            displayItems.map((item, index) => (
+              <FavItem movie={item} index={index} countSetter={setCount} counter={count} setFavorites={setFavorites} favorites={favorites}
+              priceSetter={setPrice} favoritesPrice={price}/> // replace with BakeryItem component
+              // TODO: map bakeryData to BakeryItem components
+            ))
+          } */}
+          else{displayItems.map((item, index) => (
+            <BakeryItem movie={item} index={index} countSetter={setCount} counter={count} setFavorites={setFavorites} favorites={favorites}
+            priceSetter={setPrice} favoritesPrice={price}/> // replace with BakeryItem component
+            // TODO: map bakeryData to BakeryItem components
+          ))}
+        </div> 
+      </div>
+
+      <div>
+        <h2>Cart</h2>
+        {<h2>Current count: {count}</h2>}
+        {<h2>Favorites: {favorites}</h2>}
+        {<h2>Total Price of Favorites: {price}</h2>
+        /* TODO: render a list of items in the cart */}
+      </div>
     </div>
   );
 }
---
+++ src/components/FavItem.js
@@ -0,0 +1,60 @@
+import "./BakeryItemCss.css";
+import { useEffect, useState } from "react";
+export default function BakeryItem({movie, index, countSetter, counter, setFavorites, favorites, priceSetter, favoritesPrice,
+    }){
+        //buttonTxtSetter, buttonTxt
+        const [buttonTxt, buttonTxtSetter] = useState("Remove from Favorites");
+        function handleClick() {
+            //aggregator needs to get updated to account for only what is on dispaly
+            if(buttonTxt==="Add to Favorites"){
+                buttonTxtSetter("Remove from Favorites");
+                setFavorites([...favorites, movie.name]) ;
+                priceSetter(favoritesPrice+movie.price)
+            }
+            else{
+                buttonTxtSetter("Add to Favorites");
+                const new_list = favorites.filter(function (x) {
+                    return x !== movie.name;
+                });
+                setFavorites(new_list) ;
+                priceSetter(favoritesPrice-movie.price)
+            }
+          }
+    return(
+        <div class="card">
+        <div>
+            <div className="words">
+                <br></br>
+                <h3>({index+1}) {movie.name}</h3>
+            </div>
+            <div className="words">
+            <img src= {movie.image} width="190vw"/>
+            <br></br>
+            Price: ${movie.price}
+            {/* let ifFree;
+            if ({movie.price}==0){
+                <h2>FREE</h2>
+            } */}
+            </div>
+            <div className="words">
+            Views: {movie.views}
+            </div>
+            <div className="words">
+            Rated: {movie.rated}
+            </div>
+            <div className="description">
+                {movie.description}
+            </div>
+            <div className="words">
+            <button onClick={() => { 
+                handleClick();
+                //countSetter(counter+1)
+                // setCart([...cart_items, movie.name]) ;
+                //priceSetter(counter_price+movie.price)
+            }}>{buttonTxt}</button>
+            <br></br>
+            </div>
+        </div>
+        </div>
+    )
+}
